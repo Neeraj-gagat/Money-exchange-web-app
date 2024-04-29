bf8a2ca8cf07bcb6703d1f2fa773d651d9422840
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-04-29 23:53:33 +0530

    initial commit

---
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -0,0 +1,54 @@
+import { getServerSession } from "next-auth";
+import { SendCard } from "../../../components/SendMoney";
+import { authOptions } from "../../lib/auth";
+import prisma from "@repo/db/client";
+import { BalanceCard } from "../../../components/BalanceCard";
+import { P2PTransaction } from "../../../components/P2PTransactions";
+
+async function getBalance() {
+    const session = await getServerSession(authOptions);
+    const balance = await prisma.balance.findFirst({
+        where: {
+            userId: Number(session?.user?.id)
+        }
+    });
+    return {
+        amount: balance?.amount || 0,
+        locked: balance?.locked || 0
+    }
+}
+
+async function getP2POnRampTransactions() {
+    const session = await getServerSession(authOptions);
+    const txns = await prisma.p2pTransfer.findMany({
+        where: {
+            fromUserId: Number(session?.user?.id)
+        }
+    });
+    return txns.map(t => ({
+        time: t.timestamp,
+        amount: t.amount,
+    }))
+}
+
+export default async function() {
+    const balance = await getBalance();
+    const transactions = await getP2POnRampTransactions();
+
+    return <div className="w-full">
+        <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
+        P2P Transfer
+        </div>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
+            <div >
+                <SendCard />
+            </div>
+            <div>
+                <BalanceCard amount={balance.amount} locked={balance.locked}/>
+                <div className="pt-4">
+                    <P2PTransaction transactions={transactions} />
+                </div>
+            </div>
+        </div>
+    </div>
+}
